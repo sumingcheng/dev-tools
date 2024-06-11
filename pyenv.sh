561933db6e66f60e49e6c8596160bc93283aa357
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-06-11 17:44:04 +0800

    默认 bash

--- pyenv.sh
+++ pyenv.sh
@@ -10,3 +10,16 @@ index_main() {
 }
 
 index_main
+
+
+# 配置环境变量
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
+# 使更改生效
+source ~/.bashrc
+
+# 显示 pyenv 版本
+pyenv --version
