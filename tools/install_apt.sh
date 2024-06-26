6bd7fc49b9b79b1d564220ac6718f50acc5c14ad
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-06-26 11:00:41 +0800

    chore: Update apt package installation script to use JSON file

--- tools/install_apt.sh
+++ tools/install_apt.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# 检查文件是否存在
-if [ ! -f apt-list.txt ]; then
-    echo "找不到文件 apt-list.txt，脚本终止。"
+handle_error() {
+    echo "$1"  # 传入错误信息作为参数
     exit 1
+}
+
+# 检查并安装 jq
+if ! command -v jq &>/dev/null; then
+    echo "jq 未安装，正在安装 jq..."
+    sudo apt-get update && sudo apt-get install -y jq || handle_error "jq 安装失败，脚本终止。"
+fi
+
+# 检查文件是否存在
+if [ ! -f apt-list.json ]; then
+    handle_error "找不到文件 apt-list.json，脚本终止。"
 fi
 
 # 更新软件包索引
-if ! sudo apt-get update; then
-    echo "软件包列表更新失败，脚本终止。"
-    exit 1
-fi
+sudo apt-get update || handle_error "软件包列表更新失败，脚本终止。"
 
 # 初始化计数器和失败包的列表
 total_packages=0
@@ -18,28 +25,19 @@ success_count=0
 fail_count=0
 failed_packages=()
 
-# 读取apt-list.txt文件中的软件包列表
-while read -r pkg; do
-    if [ -n "$pkg" ]; then  # 确保不是空行
-        ((total_packages++))
-        echo "准备安装 $pkg..."
-        # 安装软件包并允许用户交互
-        if sudo apt-get install -y $pkg; then
-            ((success_count++))
-        else
-            ((fail_count++))
-            failed_packages+=($pkg)
-        fi
+# 从JSON文件中读取软件包名称并存储到数组中
+mapfile -t packages < <(jq -r '.[].name' apt-list.json)
+
+# 安装软件包
+for pkg in "${packages[@]}"; do
+    echo "准备安装 $pkg..."
+    if sudo apt-get install -y "$pkg"; then
+        ((success_count++))
+    else
+        ((fail_count++))
+        failed_packages+=("$pkg")
     fi
-done < apt-list.txt
+done
 
 # 输出安装总结
-echo "安装过程完成。"
-echo "一共尝试安装 $total_packages 个包。"
-echo "成功安装 $success_count 个包。"
-echo "安装失败 $fail_count 个包。"
-
-if [ $fail_count -gt 0 ]; then
-    echo "以下包安装失败："
-    printf '%s\n' "${failed_packages[@]}"
-fi
+echo "尝试安装 $total_packages 个包：成功 $success_count 个，失败 $fail_count 个。失败的包：${failed_packages[*]}"
