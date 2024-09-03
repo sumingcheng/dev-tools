204651e621550a7ff0f60b1b970c79b7fca38dfc
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-09-03 14:12:49 +0800

    chore: 更新 nginx.conf 的 log_format，包括 $time_iso8601 和提高可读性

--- sh/anaconda/unInstall.sh
+++ sh/anaconda/unInstall.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# 确认卸载
-read -p "确定要卸载 Anaconda 吗? 这将移除/opt/anaconda3 目录和相关配置。[y/n]: " confirm
+read -p "确定要卸载 Anaconda 吗? 这将移除 /opt/anaconda3 目录和相关配置。[y/n]: " confirm
 if [[ "$confirm" != [Yy] ]]; then
     echo "取消卸载"
     exit 0
 fi
 
-# 检查 Anaconda 安装目录是否存在
 if [ -d "/opt/anaconda3" ]; then
     echo "正在移除 Anaconda 目录..."
     sudo rm -rf "/opt/anaconda3"
@@ -15,7 +13,6 @@ else
     echo "未找到 Anaconda 目录，可能已经被移除。"
 fi
 
-# 清理环境变量配置
 echo "清理环境变量配置..."
 if [ -f "/etc/profile.d/conda.sh" ]; then
     sudo rm "/etc/profile.d/conda.sh"
@@ -23,5 +20,17 @@ if [ -f "/etc/profile.d/conda.sh" ]; then
 else
     echo "未找到环境初始化文件 /etc/profile.d/conda.sh，可能已经被移除。"
 fi
+
+for file in $HOME/.bashrc $HOME/.bash_profile $HOME/.zshrc; do
+    if [ -f "$file" ]; then
+        echo "正在从 $file 中移除 Conda 配置..."
+        sed -i '/conda init/d' $file
+        sed -i '/conda activate/d' $file
+    fi
+done
+
+echo "正在移除隐藏的 Conda 目录..."
+rm -rf $HOME/.conda
+rm -rf $HOME/.continuum
 
 echo "Anaconda 卸载完成。"
