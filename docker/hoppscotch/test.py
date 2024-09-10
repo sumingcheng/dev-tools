ba7ddfac311d464b3709122729d96551ed80b415
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-09-10 14:14:14 +0800

    fix: hoppscotch

--- docker/hoppscotch/test.py
+++ docker/hoppscotch/test.py
@@ -1,10 +1,19 @@
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 import logging
+from smtplib import SMTPException
 
-# 设置日志配置
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+# 配置日志
+def configure_logging():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+configure_logging()
+
+# 从环境变量中获取SMTP服务器信息
+SMTP_HOST = os.getenv('SMTP_HOST', '172.22.220.64')
+SMTP_PORT = int(os.getenv('SMTP_PORT', 1025))
 
 # 创建一个MIMEText邮件对象，设置文本内容和字符集为utf-8
 message = MIMEText('这是通过MailCatcher发送的测试邮件内容。', 'plain', 'utf-8')
@@ -12,20 +21,18 @@ message['From'] = Header('from@example.com', 'utf-8')
 message['To'] = Header('to@example.com', 'utf-8')
 message['Subject'] = Header('测试邮件', 'utf-8')
 
-# 连接到SMTP服务器
-server = smtplib.SMTP('172.22.220.64', 1025)
-
+# 尝试发送邮件并管理SMTP连接
 try:
-    # 发送邮件
-    server.sendmail(
-        'from@example.com',
-        ['to@example.com'],
-        message.as_string()  # 将MIMEText对象转换成字符串
-    )
+    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
+        server.sendmail(
+            'from@example.com',
+            ['to@example.com'],
+            message.as_string()  # 将MIMEText对象转换成字符串
+        )
     logging.info('邮件发送成功')
+except SMTPException as e:
+    logging.error(f'SMTP错误: {e}')
 except Exception as e:
-    logging.error(f'邮件发送失败: {e}')
+    logging.error(f'其他错误: {e}')
 finally:
-    # 关闭SMTP服务器连接
-    server.quit()
     logging.info('SMTP连接已关闭')
