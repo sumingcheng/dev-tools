85f0c2a6f9cfca05c3c36e840ebdd7f3f6d97990
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-09-19 11:05:23 +0800

    add: sh

---
+++ docker/halo/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 设置必要的目录路径
+data_dir="/data/dev-tools/docker/sftpGo/data"
+config_dir="/data/dev-tools/docker/sftpGo/config"
+
+# 检查并创建数据和配置目录
+echo "检查并创建数据目录..."
+mkdir -p $data_dir
+mkdir -p $config_dir
+
+# 设置目录权限
+echo "设置目录权限..."
+chown 1100:1100 $data_dir
+chown 1100:1100 $config_dir
+
+# 输出目录状态
+echo "输出目录权限和所有权状态..."
+ls -lnd $data_dir
+ls -lnd $config_dir
+
+# 运行 Docker Compose
+echo "启动 Docker Compose..."
+docker-compose up -d
