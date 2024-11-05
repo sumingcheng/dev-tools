f1c81d6a602608b69a3948906c6090fba26b15e1
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-11-05 09:40:41 +0800

    harbor

--- sh/harbor/install.sh
+++ sh/harbor/install.sh
@@ -42,7 +42,7 @@ chmod +x ./harbor/install.sh || { echo "无法给 install.sh 添加执行权限�
 ./harbor/install.sh || { echo "Harbor 安装失败，退出。"; exit 1; }
 
 # 启动 Harbor
-docker-compose -f ./docker-compose.yml -f ./harbor/docker-compose.yml up -d || { echo "启动 Harbor 失败，退出。"; exit 1; }
+docker-compose -f ./harbor/docker-compose.yml up -d || { echo "启动 Harbor 失败，退出。"; exit 1; }
 
 # 提示用户
 echo "Harbor 已下载并解压。"
