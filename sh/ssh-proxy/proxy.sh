4d563b3d1f35218ded2c5fb8731936ddf60d6fc0
sumingcheng <cjwdyzdyplxjg@gmail.com>
2025-02-13 19:28:56 +0800

    设置脚本

--- sh/ssh-proxy/proxy.sh
+++ sh/ssh-proxy/proxy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# 显示使用说明
-echo "代理设置工具"
-echo "设置代理：source proxy.sh set"
-echo "取消代理：source proxy.sh unset"
-echo "----------------------------------------"
-
 # 函数：显示使用方法
 usage() {
     echo "Usage: source $0 [set|unset]"
     echo "请确保使用 'source' 或 '.' 命令执行此脚本以影响当前 shell 环境。"
+    echo "代理设置工具"
+    echo "设置代理：source proxy.sh set"
+    echo "取消代理：source proxy.sh unset"
+    echo "----------------------------------------"
     return
 }
 
@@ -28,14 +26,14 @@ fi
 # 设置或取消代理
 case $1 in
     set)
-        read -p "请输入代理地址（格式如 http://127.0.0.1:7890）: " proxy_address
-        if [ -z "$proxy_address" ]; then
+        read -p "请输入代理地址（格式如 192.168.1.3:7890）: " proxy_input
+        if [ -z "$proxy_input" ]; then
             echo "错误：代理地址不能为空"
             return 1
         fi
+        proxy_address="http://$proxy_input"
         export HTTP_PROXY="$proxy_address"
         export HTTPS_PROXY="$proxy_address"
-        # 为当前 shell 设置代理
         export all_proxy="$proxy_address"
         echo "HTTP/HTTPS 代理已设置为 $proxy_address"
     ;;
